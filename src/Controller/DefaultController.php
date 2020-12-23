7af20a1470ac5220bbe6f22c879630ab890f788f
nbaccour <46217602+nbaccour@users.noreply.github.com>
2020-12-23 18:44:55 +0100

    premiere page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msi-n
+ * Date: 23/12/2020
+ * Time: 18:22
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+
+        dump('ça fonctionne');
+        die();
+    }
+
+}
