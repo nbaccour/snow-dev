83413d6bbd30747a3017bd54b5098c9e1477de7d
nbaccour <46217602+nbaccour@users.noreply.github.com>
2020-12-28 23:31:29 +0100

    creation entity user et authentificator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,26 +4,52 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Trick;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
 
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
     {
+
+        $faker = \Faker\Factory::create('FR-fr');
+
+        $admin = new User();
+        $hash = $this->encoder->encodePassword($admin, "password");
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
+
         $categoryName = ['Grabs', 'Rotations', 'Flips', 'Rotations désaxées', 'Slides', 'One foot'];
         $tricksName = ['Mute', 'Indy', '360', '720', 'Backflip', 'Misty', 'Tail slide', 'Method air', 'Backside air'];
 
-        $faker = \Faker\Factory::create('FR-fr');
+
         $aCategory = [];
         foreach ($categoryName as $name) {
 
@@ -40,7 +66,7 @@ class AppFixtures extends Fixture
 
                 $trick = new Trick();
                 $trick->setName($faker->randomElement($tricksName))
-                    ->setDescription($faker->sentence())
+                    ->setDescription($faker->paragraph(5))
                     ->setSlug(strtolower($this->slugger->slug($trick->getName())))
                     ->setCreatAt($faker->dateTimeBetween('-6 months'))
                     ->setPicture('img/tricks/img' . $faker->numberBetween(1, 39) . '.jpg')
